93b0c0a1da6b9832ac9cb95522ccb63c794638a9
Rafael Piazdecki <rafael.piazdecki@b2net.pl>
2016-07-10 01:22:50 +0200

    changes in TodoList class:
    - added setters and getter for title and items properties
    - added constructor

--- todolist.rb
+++ todolist.rb
@@ -1,5 +1,11 @@
 class TodoList
-    # methods and stuff go here
+
+  attr_reader :title, :items
+
+  def initialize(list_title)
+    @title = list_title
+    @items = Array.new # Starts empty! No Items yet!
+  end
 end
 
 class Item
